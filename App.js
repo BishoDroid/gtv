9c0aaeccca5b726f9f97e4cf8ea254c98632bbbd
Socheat KHAUV <pkayjava@gmail.com>
2018-10-04 14:53:08 +0800

    added the push notification to app.js

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {TextInput, Keyboard} from 'react-native';
+import {Constants, Notifications, Permissions} from 'expo';
+
+
 import {
     AppRegistry,
     StyleSheet,
@@ -9,8 +13,49 @@ import {
 
 import Main from './src/components/Main';
 
+
+
 export default class loginAnimation extends Component {
+
+    sendBellowMinimumValueNotification() {
+        Keyboard.dismiss();
+
+        const localNotification = {
+            title: 'Balance is bellow minimum value',
+            body: 'The Balance is bellow the minimum value, please take necessary actions!'
+        };
+
+        const schedulingOptions = {
+            time: new Date().getTime() + 1000
+        }
+
+        // Notifications show only when app is not active.
+        // (ie. another app being used or device's screen is locked)
+        Notifications.scheduleLocalNotificationAsync(
+            localNotification, schedulingOptions
+        );
+    }
+
+    handleNotification() {
+
+}
+
+    async componentDidMount() {
+        // We need to ask for Notification permissions for ios devices
+        let result = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+
+        if (Constants.isDevice && result.status === 'granted') {
+            console.log('Notification permissions granted.')
+
+        }
+
+        // If we want to do something with the notification when the app
+        // is active, we need to listen to notification events and
+        // handle them in a callback
+        Notifications.addListener(this.handleNotification);
+}
     render() {
+   this.sendBellowMinimumValueNotification();
         return (
             <View style={styles.container}>
                 <Main />
@@ -18,6 +63,7 @@ export default class loginAnimation extends Component {
         );
     }
 }
+
 
 const styles = StyleSheet.create({
     container: {
